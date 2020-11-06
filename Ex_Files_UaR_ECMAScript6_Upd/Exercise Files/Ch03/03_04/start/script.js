817c7a08127dd7556b0e3fe267e9914b0bd08889
Josh Mann <jlmann72@gmail.com>
2020-11-06 11:24:08 -0500

    Spread Operator

---
+++ Ex_Files_UaR_ECMAScript6_Upd/Exercise Files/Ch03/03_04/start/script.js
@@ -0,0 +1,7 @@
+
+
+var cats = ["Tabby", "Siamese", "Persian"];
+var dogs = ["Golden Retriever", "Pub", "Australian Shepherd"];
+var animals = ["Whale", "Giraffe", ...cats, "Snake", ...dogs, "Coyote"];
+
+console.log(animals);
