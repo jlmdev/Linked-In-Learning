4ae5cf91c8e4438bb0f809cbc3f535e144a2756d
Josh Mann <jlmann72@gmail.com>
2020-11-06 16:02:50 -0500

    Arrow functions

--- Ex_Files_UaR_ECMAScript6_Upd/Exercise Files/Ch04/04_03/start/script.js
+++ Ex_Files_UaR_ECMAScript6_Upd/Exercise Files/Ch04/04_03/start/script.js
@@ -1,23 +1,7 @@
-var books = new Set();
-books.add('The Hobbit')
-.add('Lord of the Rings')
-.add('Dune');
+// var studentList = function(students) {
+//   console.log(students);
+// };
 
-console.log(books);
-console.log(`How many books? ${books.size}`);
-console.log(`Has Dune? ${books.has('Dune')}`);
+var studentList = (students) => console.log(students);
 
-var data = [4,2,4,4,2,5,1,6,7,5,6,8,2,7];
-var set = new Set(data);
-console.log('data.length', data.length);
-console.log('set.size', set.size);
-
-var topics = new Map();
-topics.set('HTML', '/class/html');
-topics.set('CSS', '/class/CSS');
-topics.set('JavaScript', '/class/JavaScript');
-topics.set('Node', '/class/Node');
-
-for (let topic of topics.keys()) {
-  console.log(`${topic} is the course name`);
-}
+studentList(["Joe", "Cindy", "Jeanne"]);
