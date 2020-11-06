75a025f970eee1a8d1af6dd745513bfd59c5d5e2
Josh Mann <jlmann72@gmail.com>
2020-11-06 11:39:42 -0500

    Maps

---
+++ Ex_Files_UaR_ECMAScript6_Upd/Exercise Files/Ch03/03_05/start/script.js
@@ -0,0 +1,16 @@
+var course = new Map();
+course.set('react', {description: 'ui'});
+course.set('jest', {description: 'testing'});
+
+console.log(course);
+console.log(course.get('react'));
+
+var details = new Map([
+  [new Date(), 'today'],
+  ['items', [1, 2]]
+]);
+
+console.log(details.size);
+details.forEach(function(item) {
+  console.log(item);
+});
