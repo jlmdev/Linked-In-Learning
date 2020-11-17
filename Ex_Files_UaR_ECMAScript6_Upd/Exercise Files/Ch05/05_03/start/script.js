7e347064d6a6a80b626a0942c52cb5017e8d9b7f
Josh Mann <jlmann72@gmail.com>
2020-11-17 05:20:34 -0500

    tested another api for project

--- Ex_Files_UaR_ECMAScript6_Upd/Exercise Files/Ch05/05_03/start/script.js
+++ Ex_Files_UaR_ECMAScript6_Upd/Exercise Files/Ch05/05_03/start/script.js
@@ -1,13 +1,19 @@
 
-const getPeopleInSpace = () =>
-  fetch('http://api.open-notify.org/astros.json')
+// const getPeopleInSpace = () =>
+//   fetch('http://api.open-notify.org/astros.json')
+//     .then(response => response.json());
+
+// const spaceNames = () =>
+//   getPeopleInSpace()
+//     .then(json => json.people)
+//     .then(people => people.map(person => person.name))
+//     .then(names => names.join(', '));
+
+// spaceNames()
+//   .then(console.log);
+
+const getSignificantEarthquakes = () =>
+  fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson')
     .then(response => response.json());
 
-const spaceNames = () =>
-  getPeopleInSpace()
-    .then(json => json.people)
-    .then(people => people.map(person => person.name))
-    .then(names => names.join(', '));
-
-spaceNames()
-  .then(console.log);
+getSignificantEarthquakes().then(console.log);
